7fae95c08cba6fd973ecef127d98cedcf8a8546b
Rodrigo <nobrega@gmail.com>
2017-04-29 16:47:34 +0800

    Finished SIUnit()
    v1.013
    #7

--- Physics/src/HTPhysics.py
+++ Physics/src/HTPhysics.py
@@ -5,11 +5,11 @@
 # HTPhysics is an implementation of Physics
 # based on two books from Halliday and Tipler
 #
-# v1.012
+# v1.013
 # Issue #7
 #
 # Rodrigo Nobrega
-# 20170420-
+# 20170420-20170429
 #################################################
 __author__ = 'Rodrigo Nobrega'
 
@@ -21,20 +21,31 @@ class SIUnit(object):
         """Constructor"""
         self.baseUnit = {'m':'metre', 'kg':'kilogram', 's':'second'
         , 'A':'ampere', 'K':'kelvin', 'mol':'mole', 'cd':'candela'}
+        print('================================================')
+        print('SIUnit: A set of base units from SI was created.')
+        print('================================================')
+    
+    def listUnit(self):
+        """Lists all base units from SI"""
+        [print('{} : {}'.format(k, v)) for k,v in self.baseUnit.items()]
     
     def isSIunit(self, unit):
         """Tests if the unit is an SI base unit"""
         if unit in list(self.baseUnit.keys()):
-            print('OK')
+            print('Unit {} ({}) is a base unit of SI.'.format(unit, self.baseUnit[unit]))
         else:
-            print('Not OK')
+            print('Unit {} is NOT a base unit of SI.'.format(unit))
 
 
 # main loop
 def main():
+    # create an SI unit instance, with all seven SI units
     theUnit = SIUnit()
-    print(theUnit.baseUnit)
-    theUnit.isSIunit('t')
+    # print(theUnit.baseUnit)
+    # tests if a given unit is an SI unit
+    testUnit = input('Enter an unit to test: ')
+    theUnit.isSIunit(testUnit)
+    theUnit.listUnit()
 
 
 # main, calling main loop
