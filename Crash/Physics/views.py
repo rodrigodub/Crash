9ffde61c555be14404ce939188d9bc13dfc55f86
Rodrigo <nobrega@gmail.com>
2017-05-16 21:31:19 +0800

    Bootstrap installed and example page created
    # v.1.033
    # Issue #13

--- Crash/Physics/views.py
+++ Crash/Physics/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Physics Home Page')
+    # return HttpResponse('Physics Home Page')
+    return render(request, 'Physics/index.html')
