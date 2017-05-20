49bfe5b0e81f031d750184f1c6e327dc9d3aa8cb
Rodrigo <nobrega@gmail.com>
2017-05-20 09:41:31 +0800

    Base HTML template
    # v.1.039
    # Issue #20

--- Crash/Physics/views.py
+++ Crash/Physics/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     # return HttpResponse('Physics Home Page')
-    return render(request, 'Physics/index.html')
+    return render(request, 'Physics/home.html')
