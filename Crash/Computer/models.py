a18c4871d4e70f6ad7d5082ec98d830ab8e4818f
Rodrigo <nobrega@gmail.com>
2017-06-11 22:16:52 +0800

    Creating CCTransistor model
    # v.1.069
    # Issue #25

--- Crash/Computer/models.py
+++ Crash/Computer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class CCTransistor(models.Model):
+    """Class to define a Transistor with 3 electrodes"""
+    # collector
+    electrode_in = models.IntegerField(default=0)
+    # emitter
+    electrode_out = models.IntegerField(default=0)
+    # base
+    gate = models.IntegerField(default=0)
+
+    def update(self):
+        if self.gate and self.electrode_in:
+            self.electrode_out = 1
+        else:
+            self.electrode_out = 0
+
