9fc982a1643978833d1fee53792ecabc73f56bb3
Rodrigo <nobrega@gmail.com>
2017-10-10 20:54:52 +0800

    Battery created
    # v1.115
    # Issue #35

--- Python/ComputerScience/cs.py
+++ Python/ComputerScience/cs.py
@@ -4,7 +4,7 @@
 #               Computer Science
 #         CrashCourse Computer Science
 #
-# v1.114
+# v1.115
 # Issue #35
 #
 # Rodrigo Nobrega
@@ -22,24 +22,33 @@ class CCTransistor(object):
     """Class to define a Transistor with 3 electrodes."""
     def __init__(self, name):
         self.name = name
-        # collector = electrode_in
-        self.collector = 0
-        self.collectorcharge = '|'
-        # emitter = electrode_out
-        self.emitter = 0
-        self.emittercharge = '|'
-        # base = gate
-        self.base = 0
-        self.basecharge = '|'
-        print(self)
+        # collector = electrode_in. List with [state, charge]
+        self.collector = [0, '|']
+        # emitter = electrode_out. List with [state, charge]
+        self.emitter = [0, '|']
+        # base = gate. List with [state, charge]
+        self.base = [0, '|']
+        # print(self)
     
     def __str__(self):
         st = int((17 - len(self.name)) / 2) * ' ' + self.name + int((17 - len(self.name)) / 2) * ' '
         return '=================\n{}\n================='\
                 '\n   C    B    E  \n   {}    {}    {}  \n   {}    {}    {}  \n'\
                 .format(st
-                , self.collector, self.base, self.emitter
-                , self.collectorcharge, self.basecharge, self.emittercharge)
+                , self.collector[0], self.base[0], self.emitter[0]
+                , self.collector[1], self.base[1], self.emitter[1])
+    
+
+class CCBattery(object):
+    """Class to define a Battery."""
+    def __init__(self, name):
+        self.name = name
+        self.positive = [1, '+']
+        self.negative = [1, '-']
+    
+    def __str__(self):
+        return '        |\n        |   {}\n  -------------  {}\n    ---------\n        |   {}\n        |\n'\
+               .format(self.positive[1], self.name, self.negative[1])
         
 
 # test loop
@@ -47,7 +56,10 @@ def test():
     print('------------------------')
     print('Test.')
     print('------------------------')
-    a = CCTransistor('rodrigo')
+    a = CCTransistor('T1')
+    b = CCBattery('Bat1')
+    print(a)
+    print(b)
 
 
 # main loop
